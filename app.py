d2419b5936cef4b07091f4d7877f162cc4bbaf9b
Диана <lazakova.diana@mail.ru>
2024-11-30 18:12:50 +1000

    создан макет приложения

--- app.py
+++ app.py
@@ -1,11 +1,32 @@
 import dash
-from dash import dcc, html
+from dash import dcc, html, Input, Output
+import pandas as pd
+import plotly.express as px
 
+# Загрузка данных из CSV файла
+# Предположим, что файл называется "website_visits.csv"
+df = pd.read_csv('website_visits.csv')
+
+# Инициализация приложения Dash
 app = dash.Dash(__name__)
 
+# Основная структура дашборда
 app.layout = html.Div(children=[
     html.H1(children='Дашборд посещаемости веб-сайта'),
-    dcc.Graph(id='example-graph'),
+
+    # График временного ряда
+    dcc.Graph(id='time-series-graph'),
+
+    # Круговая диаграмма
+    dcc.Graph(id='pie-chart'),
+
+    # Гистограмма
+    dcc.Graph(id='histogram'),
+
+    # Таблица с данными
+    dcc.Graph(id='data-table'),
+
+    # Выпадающий список для выбора периода анализа
     dcc.Dropdown(
         id='period-dropdown',
         options=[
@@ -15,7 +36,7 @@ app.layout = html.Div(children=[
         ],
         value='month'
     ),
-])
 
-if __name__ == '__main__':
-    app.run_server(debug=True)
+    # Индикаторы для отображения текущих значений показателей
+    html.Div(id='current-indicators')
+])
